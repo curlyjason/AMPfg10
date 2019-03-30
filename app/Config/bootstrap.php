6040842940988a28808214281e340169efa41012
Don Drake <ddrake@dreamingmind.com>
2019-03-29 21:22:19 -0700

    phpunit autoload fix as recommended by cake

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -1,4 +1,21 @@
 <?php
+// TO ENABLE TESTING
+//FROM
+//https://book.cakephp.org/2.0/en/development/testing.html#run-tests-from-command-line
+//To be able to work with PHP 7, it is required to install the version 5, 
+//and you’ll need to setup the autoloader, and work around an issue in 
+//Composer’s autoloader. In your Config/bootstrap.php file add the following:
+
+// Load Composer autoload.
+require APP . 'Vendor/autoload.php';
+
+// Remove and re-prepend CakePHP's autoloader as Composer thinks it is the
+// most important.
+// See: http://goo.gl/kKVJO7
+spl_autoload_unregister(array('App', 'load'));
+spl_autoload_register(array('App', 'load'), true, true);
+
+
 /**
  * This file is loaded automatically by the app/webroot/index.php file after core.php
  *
