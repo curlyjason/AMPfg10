d44cc8a7e3aa00e911df17eb4820e5ef9c0b6239
Don Drake <ddrake@dreamingmind.com>
2019-03-29 20:01:59 -0700

    add a stub test

---
+++ AddressesControllerTest.php
@@ -0,0 +1,28 @@
+<?php
+App::uses('Controller', 'Controller');
+App::uses('View', 'View');
+App::uses('ProgressHelper', 'View/Helper');
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of AddressesControllerTest
+ *
+ * @author dondrake
+ */
+class AddressesControllerTest extends CakeTestCase
+{
+    public function setUp() {
+
+    }
+
+    public function testBar() {
+
+    }
+	//put your code here
+}
+
