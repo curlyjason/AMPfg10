b7eec45ee06fe55534c5b32ab1d55b7325c53470
dreamingmind <ddrake@dreamingmind.com>
2019-10-17 17:08:52 -0700

    Bake a controller to confirm testing bake

---
+++ app/Test/Case/Controller/CustomersControllerTest.php
@@ -0,0 +1,189 @@
+<?php
+App::uses('CustomersController', 'Controller');
+
+/**
+ * CustomersController Test Case
+ */
+class CustomersControllerTest extends ControllerTestCase {
+
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = array(
+//		'app.customer',
+//		'app.user',
+//		'app.invoice',
+//		'app.invoice_item',
+//		'app.order',
+//		'app.budget',
+//		'app.order_item',
+//		'app.item',
+//		'app.address',
+//		'app.tax_rate',
+//		'app.catalog',
+//		'app.catalogs_user',
+//		'app.image',
+//		'app.replenishment_item',
+//		'app.replenishment',
+//		'app.location',
+//		'app.cart',
+//		'app.shipment',
+//		'app.document',
+//		'app.time',
+//		'app.project',
+//		'app.observer',
+//		'app.users_user',
+//		'app.price',
+//		'app.menu',
+//		'app.user_registry',
+//		'app.preference',
+//		'app.gateway'
+	);
+
+/**
+ * testIsAuthorized method
+ *
+ * @return void
+ */
+	public function testIsAuthorized() {
+		$this->markTestIncomplete('testIsAuthorized not implemented.');
+	}
+
+/**
+ * testIndex method
+ *
+ * @return void
+ */
+	public function testIndex() {
+		$this->markTestIncomplete('testIndex not implemented.');
+	}
+
+/**
+ * testView method
+ *
+ * @return void
+ */
+	public function testView() {
+		$this->markTestIncomplete('testView not implemented.');
+	}
+
+/**
+ * testAdd method
+ *
+ * @return void
+ */
+	public function testAdd() {
+		$this->markTestIncomplete('testAdd not implemented.');
+	}
+
+/**
+ * testEditCustomer method
+ *
+ * @return void
+ */
+	public function testEditCustomer() {
+		$this->markTestIncomplete('testEditCustomer not implemented.');
+	}
+
+/**
+ * testDelete method
+ *
+ * @return void
+ */
+	public function testDelete() {
+		$this->markTestIncomplete('testDelete not implemented.');
+	}
+
+/**
+ * testSetCustomerOwnership method
+ *
+ * @return void
+ */
+	public function testSetCustomerOwnership() {
+		$this->markTestIncomplete('testSetCustomerOwnership not implemented.');
+	}
+
+/**
+ * testCreateCatalogRoot method
+ *
+ * @return void
+ */
+	public function testCreateCatalogRoot() {
+		$this->markTestIncomplete('testCreateCatalogRoot not implemented.');
+	}
+
+/**
+ * testCustomersEdit method
+ *
+ * @return void
+ */
+	public function testCustomersEdit() {
+		$this->markTestIncomplete('testCustomersEdit not implemented.');
+	}
+
+/**
+ * testFetchCustomerUserId method
+ *
+ * @return void
+ */
+	public function testFetchCustomerUserId() {
+		$this->markTestIncomplete('testFetchCustomerUserId not implemented.');
+	}
+
+/**
+ * testResetCustomerToken method
+ *
+ * @return void
+ */
+	public function testResetCustomerToken() {
+		$this->markTestIncomplete('testResetCustomerToken not implemented.');
+	}
+
+/**
+ * testTestMe method
+ *
+ * @return void
+ */
+	public function testTestMe() {
+		$this->markTestIncomplete('testTestMe not implemented.');
+	}
+
+/**
+ * testSecureRequestData method
+ *
+ * @return void
+ */
+	public function testSecureRequestData() {
+		$this->markTestIncomplete('testSecureRequestData not implemented.');
+	}
+
+/**
+ * testValidateRequestData method
+ *
+ * @return void
+ */
+	public function testValidateRequestData() {
+		$this->markTestIncomplete('testValidateRequestData not implemented.');
+	}
+
+/**
+ * testSecureSelectTakingOverTheWorld method
+ *
+ * @return void
+ */
+	public function testSecureSelectTakingOverTheWorld() {
+		$this->markTestIncomplete('testSecureSelectTakingOverTheWorld not implemented.');
+	}
+
+/**
+ * testValidateSelectTakingOverTheWorld method
+ *
+ * @return void
+ */
+	public function testValidateSelectTakingOverTheWorld() {
+		$this->markTestIncomplete('testValidateSelectTakingOverTheWorld not implemented.');
+	}
+
+}
