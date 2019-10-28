f29a56bc0157d5d45191f26085faa6bea0e59a9b
dreamingmind <ddrake@dreamingmind.com>
2019-10-28 13:19:39 -0700

    Bake a test file
    but the test was an aborted effort

--- app/Test/Case/Controller/InvoicesControllerTest.php
+++ app/Test/Case/Controller/InvoicesControllerTest.php
@@ -1,32 +1,165 @@
 <?php
-App::uses('Controller', 'Controller');
-App::uses('View', 'View');
-App::uses('ProgressHelper', 'View/Helper');
+App::uses('InvoicesController', 'Controller');
+App::uses('Invoice', 'Model');
+App::uses('ControllerTrait', 'Test/Case');
 
 /**
- * Description of InvoicesControllerTest
- *
- * @author dondrake
+ * InvoicesController Test Case
  */
-class InvoicesControllerTest extends ControllerTestCase
-{
-    public function setUp() {
+class InvoicesControllerTest extends ControllerTestCase {
 
+    use ControllerTrait;
+
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = array(
+//		'app.invoice',
+//		'app.invoice_item',
+//		'app.order',
+//		'app.user',
+//		'app.customer',
+//		'app.address',
+//		'app.tax_rate',
+//		'app.image',
+//		'app.catalog',
+//		'app.item',
+//		'app.order_item',
+//		'app.replenishment_item',
+//		'app.replenishment',
+//		'app.location',
+//		'app.cart',
+//		'app.catalogs_user',
+//		'app.price',
+//		'app.time',
+//		'app.project',
+//		'app.observer',
+//		'app.budget',
+//		'app.users_user',
+//		'app.shipment',
+//		'app.document',
+//		'app.menu',
+//		'app.user_registry',
+//		'app.preference',
+//		'app.gateway'
+	);
+
+/**
+ * testIsAuthorized method
+ *
+ * @return void
+ */
+	public function testIsAuthorized() {
+		$this->markTestIncomplete('testIsAuthorized not implemented.');
+	}
+
+/**
+ * testIndex method
+ *
+ * @return void
+ */
+	public function testIndex() {
+		$this->markTestIncomplete('testIndex not implemented.');
+	}
+
+/**
+ * testView method
+ *
+ * @return void
+ */
+	public function testView() {
+		$this->markTestIncomplete('testView not implemented.');
+	}
+
+/**
+ * testAdd method
+ *
+ * @return void
+ */
+	public function testAdd() {
+		$this->markTestIncomplete('testAdd not implemented.');
+	}
+
+/**
+ * testEdit method
+ *
+ * @return void
+ */
+	public function testEdit() {
+		$this->markTestIncomplete('testEdit not implemented.');
+	}
+
+/**
+ * testDelete method
+ *
+ * @return void
+ */
+	public function testDelete() {
+		$this->markTestIncomplete('testDelete not implemented.');
+	}
+
+/**
+ * testListInvoices method
+ *
+ * @return void
+ */
+	public function testListInvoices() {
+		$this->markTestIncomplete('testListInvoices not implemented.');
+	}
+
+/**
+ * testInvoice method
+ *
+ * @return void
+ */
+	public function testInvoice() {
+		$this->markTestIncomplete('testInvoice not implemented.');
+	}
+
+/**
+ * testSubmitInvoice method
+ *
+ * @return void
+ */
+    public function testSubmitInvoice() {
+        $this->markTestIncomplete('testSubmitInvoice not implemented.');
     }
 
-    public function testBar() {
-
+    /**
+     * testViewOldInvoice method
+     *
+     * @return void
+     */
+    public function testViewOldInvoice() {
+        $this->markTestIncomplete('testSubmitInvoice not implemented.');
     }
-	
-	public function testViewOldInvoice()
-	{
-		
+
+    /**
+     * Don't know how to resolve rendering/event problems
+     */
+	public function testSaveInvoiceNumberSuccess() {
+        /** @var InvoicesController $controller */
+//	    $controller = $this->makeController('Invoices');
+//	    $controller->Invoice = $this->getMockForModel('Invoice');
+//        $controller->Invoice
+//            ->method('save')
+//            ->willReturn(true);
+//
+//	    $result = $controller->saveInvoiceNumber('1', 'ABC123');
+//	    $expected = json_encode(['response' => TRUE]);
+//
+//		$this->assertEquals($result == $expected, 'A successful save did not return the expected json');
 	}
-	
-	public function testViewOldInvoiceStatus()
-	{
-		
+
+/**
+ * testSaveInvoiceNumber method
+ *
+ * @return void
+ */
+	public function testSaveInvoiceNumberFail() {
+		$this->markTestIncomplete('testSaveInvoiceNumber not implemented.');
 	}
-	
+
 }
-
